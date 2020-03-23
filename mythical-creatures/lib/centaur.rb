0da28a1d38b0bf4d4775aef6b71fb11389e660eb
Max Mitrani <mjmitrani168@gmail.com>
2020-03-23 10:02:09 -0600

    Add Shoot and run methods

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -1,10 +1,19 @@
 require 'pry'
 
 class Centaur
-
+attr_reader :name, :breed
  def initialize(name, breed)
    @name = name
    @breed = breed
  end
+
+ def shoot
+    p "Twang!!!"
+ end
+
+ def run
+    p "Clop clop clop clop!!!"   
+ end
+
 
 end
